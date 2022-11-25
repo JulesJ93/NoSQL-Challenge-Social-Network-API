c54ba8dca48ae114f2a27c3166e78963ad727890
Jules Joseph <julesj93@gmail.com>
2022-11-25 15:13:02 -0500

    adding schema for user database

---
+++ models/user.js
@@ -0,0 +1,10 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      require: true,
+      trim: true,
+    },
