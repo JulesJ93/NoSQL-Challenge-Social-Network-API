018e026bc6fb7e0e799b75e1cf4cba3c7837f6d4
Jules Joseph <julesj93@gmail.com>
2022-11-25 14:58:07 -0500

    adding add reaction to thought controller

--- controllers/thought-controllers.js
+++ controllers/thought-controllers.js
@@ -88,3 +88,34 @@ updateThought({ params, body }, res) {
     })
     .catch(err => res.json(err));
   },
+
+   //Add a reaction to a thought
+   addReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $push: { reactions: body} },
+      { new: true, runValidators: true }
+    )
+    .then(dbThoughtData => {
+      if (!dbThoughtData) {
+        res.status(404).json({ message: 'This id does not return a Thought! '});
+        return;
+      }
+      res.json(dbThoughtData)
+    })
+    .catch(err => res.json(err));
+  },
+
+  //Delete a reaction
+  removeReaction({ params }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $pull: { reactions: { reactionId: params.reactionId } } },
+      { new: true }
+    )
+    .then(dbThoughtData => res.json(dbThoughtData))
+    .catch(err => res.json(err));
+  }
+};
+
+module.exports = thoughtController;
