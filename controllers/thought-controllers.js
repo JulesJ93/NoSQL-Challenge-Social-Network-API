8befcc6000a290f76b5f2c301b72092493760737
Jules Joseph <julesj93@gmail.com>
2022-11-25 14:56:33 -0500

    adding additional code for thought controller

--- controllers/thought-controllers.js
+++ controllers/thought-controllers.js
@@ -34,5 +34,30 @@ const thoughtController = {
       }
       res.json(dbThoughtData);
     })
-
-    
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+    },
+  
+     // Add a thought to a user
+     addThought({ params, body }, res) {
+      console.log(body);
+      Thought.create(body)
+        .then(({ _id }) => {
+          return User.findOneAndUpdate(
+            { username: body.username },
+            { $push: { thoughts: _id } },
+            { new: true }
+          );
+        })
+        .then(dbUserData => {
+          if (!dbUserData) {
+            res.status(404).json({ message: 'This id does not return a User!' });
+            return;
+          }
+          res.json(dbUserData);        
+        })
+        .catch(err => res.json(err));
+    },
+  
