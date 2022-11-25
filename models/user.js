2faefc7a65c711f476ae3dc88832ff8756446f36
Jules Joseph <julesj93@gmail.com>
2022-11-25 15:13:35 -0500

    adding additional code for database model validator

--- models/user.js
+++ models/user.js
@@ -8,3 +8,35 @@ const userSchema = new Schema(
       require: true,
       trim: true,
     },
+    email: {
+        type: String,
+        require: true,
+        unique: true,
+        match: [/.+@.+\..+/, "Not a valid email"],
+      },
+      thoughts: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "Thought",
+        },
+      ],
+  
+      friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "User",
+        },
+      ],
+    },
+    {
+        toJSON: {
+          virtuals: true,
+        },
+        id: false,
+      }
+    );
+    
+    userSchema.virtual("friendCount").get(function(){return this.friends.length});
+    const User = model('User', userSchema);
+    
+    module.exports = User;
