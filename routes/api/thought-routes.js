f76327396ff5a657d65d44d63f824c4ab7103c9a
Jules Joseph <julesj93@gmail.com>
2022-11-25 15:16:46 -0500

    creating const router to get user interactions

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+
+const {
+  getAllThoughts,
+  getThoughtById,
+  addThought,
+  updateThought,
+  removeThought,
+  addReaction,
+  removeReaction,
+} = require("../../controllers/thought-controller");
