be5a01757d18b6d71134e2a29c13faa098b6f4a1
Jules Joseph <julesj93@gmail.com>
2022-11-25 15:02:38 -0500

    create a user delete user update user ect

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -16,7 +16,6 @@ const userController = {
       });
     },
 
-    
     //get on user by id
     getUserById({ params }, res) {
         User.findOne({ _id: params.id })
@@ -42,3 +41,37 @@ const userController = {
           res.status(400).json(err);
         });
       },
+
+       //create a User
+    createUser({ body }, res) {
+        User.create(body)
+          .then(dbUserData => res.json(dbUserData))
+          .catch(err => res.status(400).json(err));
+      },
+    
+      //update a user by id
+      updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, {new: true, runValidators: true })
+          .then(dbUserData => {
+            if (!dbUserData) {
+              res.status(404).json({ message: 'This id does not return a User!' });
+              return;
+            }
+            res.json(dbUserData);
+          })
+          .catch(err => res.status(400).json(err));
+      },
+    
+      //delete User
+      deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+          .then(dbUserData => {
+            if (!dbUserData) {
+              res.status(404).json({ message: 'This id does not return a User!' });
+              return;
+            }
+            res.json(dbUserData);
+          })
+          .catch(err => res.status(400).json(err));
+      },
+    
