9e3f3677a27a6d1cd1a3ec47e82db4e152dda069
Jules Joseph <julesj93@gmail.com>
2022-11-25 15:00:46 -0500

    adding user controller  to get user by and create a user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -17,3 +17,28 @@ const userController = {
     },
 
     
+    //get on user by id
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+        .populate({
+          path: 'thoughts',
+          select: '-__v'
+        })
+        .populate({
+          path: 'friends',
+          select: '-__v'
+        })
+        .select('-__v')
+        .then(dbUserData => {
+          // send 404 if no user found
+          if (!dbUserData) {
+            res.status(404).json({ message: 'This id does not return a User!' });
+            return;
+          }
+          res.json(dbUserData);
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(400).json(err);
+        });
+      },
