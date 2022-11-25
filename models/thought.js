bc67be94ccdcb1fa0a09f0f24a92f32089602b28
Jules Joseph <julesj93@gmail.com>
2022-11-25 15:07:25 -0500

    adding schema models for database for front end

---
+++ models/thought.js
@@ -0,0 +1,51 @@
+const { Schema, Types, model } = require("mongoose");
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      get : secs => new Date(secs).toLocaleDateString()
+    }
+}
+)
+
+// const { Schema, model } = require("mongoose");
+
+// const reactionSchema = new Schema(
+//   {
+//     reactionId: {
+//         type: Schema.Types.ObjectId,
+//         default: () => new Types.ObjectId()
+//     },
+//     reactionBody: {
+//         type: String,
+//         required: true,
+//         maxLength: 280
+//     },
+//     username: {
+//         type: String,
+//         required: true
+//     },
+//     createdAt: {
+//       type: Date,
+//       default: Date.now(),
+//       get : secs => new Date(secs).toLocaleDateString()
+//     }
+// }
+// )
+
+
